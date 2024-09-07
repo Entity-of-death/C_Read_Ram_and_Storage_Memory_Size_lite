05cb3dcea743e61a634272a0a34af3e52b140867
Nevermind <entity-of-death@tutanota.de>
2024-09-07 12:17:48 +0200

    updated license comment in header file

--- include/dffos.h
+++ include/dffos.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 ???Nevermind!!!
+ * Copyright (C) 2024 QM-Nevermind
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License (LGPL) Version 3
