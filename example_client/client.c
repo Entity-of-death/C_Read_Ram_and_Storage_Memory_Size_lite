ae4181b3d478c98dbbaa9deae882e0ded23b0a6a
Nevermind <entity-of-death@tutanota.de>
2024-10-04 10:05:00 +0200

    Don't use the GB unit. it is too coarse

--- example_client/client.c
+++ example_client/client.c
@@ -2,8 +2,8 @@
 #include "../include/dffos.h"
 
 int main(){
-    printf("Total RAM size:%luGB\nfree RAM size:%luGB\n", get_total_ram(3), get_free_ram(3));
-    printf("Total Drive size::%lluGB\nfree drive size:%lluGB\n",get_total_storage_memory(3), get_free_storage_memory(3));
+    printf("Total RAM size:%luMB\nfree RAM size:%luMB\n", get_total_ram(2), get_free_ram(2));
+    printf("Total Drive size::%lluMB\nfree drive size:%lluMB\n",get_total_storage_memory(2), get_free_storage_memory(2));
     getchar();
     return 0;
 }
